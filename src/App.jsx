2c3b8b2dcbdab49e1b2844fdf22359c47fd3609f
gnalmeida <gnalmeida@ADESP-FEDORA>
2024-07-25 08:48:58 -0300

    feat: Finalizado funcionalidades basicas

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ export function App() {
           {posts.map(post => {
             return (
               <Post 
-                key={post.id}
+                key={post.id} 
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,24 @@
 import { Trash, ThumbsUp } from '@phosphor-icons/react';
-import styles from './Comment.module.css';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
 
-export function Comment(){
+
+
+export function Comment({ content, onDeleteComment }) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -15,17 +31,17 @@ export function Comment(){
                             <time title="21 de Julho às 12:00h" dateTime="2024-07-21 13:00:00"> Cerca de 45min atrás</time>
                         </div>
 
-                        <button title="Deletar Comentário">
+                        <button onClick={handleDeleteComment} title="Deletar Comentário">
                             <Trash size={24} />
                         </button>
                     </header>
 
-                    <p>Muito bom Gabriel, parabéns!!</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
